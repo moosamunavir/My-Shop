d0aa42d18ce56aa582db66a390b0a2c61cfdf8c0
unknown <moosakpmunavir@gmail.com>
2024-11-05 06:17:40 +0530

    Add all Files

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,12 @@ import cookieParser from "cookie-parser";
 import { connectDatabase } from "./config/dbConnect.js";
 import errorMiddleware from "./middleWares/error.js";
 
+import path from "path";
+//import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // handle uncaught excemptions errors
 
 process.on("uncaugthExcemption", (err) => {
@@ -37,11 +43,20 @@ import productRoutes from "./routes/products.js";
 import authRoutes from "./routes/auth.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
+import { fileURLToPath } from "url";
 
 app.use("/api/v1", productRoutes);
 app.use("/api/v1", authRoutes);
 app.use("/api/v1", orderRoutes);
 app.use("/api/v1", paymentRoutes);
+
+if (process.env.NODE_ENV === "PRODUCTION") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"))
+  })
+}
 
 // Using Error middleware
 
--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,7 +4,9 @@ import products from "./data.js";
 
 const seedProduct = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/shopit-v2");
+    await mongoose.connect(
+      "mongodb+srv://moosakpmunavir:1JKqXNrqyT3gb8h8@shopit.0iulm.mongodb.net/?retryWrites=true&w=majority&appName=shopit"
+    );
 
     await product.deleteMany();
     console.log("products are deleted");
